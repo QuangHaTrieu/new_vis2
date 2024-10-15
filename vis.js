53d438f648aae17bfe3db3da59b859508f428917
QuangHaTrieu <145729774+QuangHaTrieu@users.noreply.github.com>
2024-10-15 21:46:00 +1100

    Add files via upload

---
+++ vis.js
@@ -0,0 +1,22 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var heat_chart = "heatmap.vg.json";
+vegaEmbed("#heat", heat_chart).then(function(result) {
+}).catch(console.error);
+
+var donut_chart = "donut_chart.vg.json";
+vegaEmbed("#donut", donut_chart).then(function(result) {
+}).catch(console.error);
+
+var slope_chart = "slope.vg.json";
+vegaEmbed("#slope", slope_chart).then(function(result) {
+}).catch(console.error);
+
+var parallel_chart = "parallel.vg.json";
+vegaEmbed("#parallel", parallel_chart).then(function(result) {
+}).catch(console.error);
+
+
+
